349c1ab1e32a21e2bca0627fa04b96345446e12f
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-08-04 11:03:54 -0400

    basic logic completed

--- app.js
+++ app.js
@@ -8,21 +8,12 @@ app.use(cors({ optionSuccessStatus: 200 }));
 app.use(express.static("public"));
 app.get("/", (req, res) => res.sendFile(`${__dirname}/views/index.html`));
 
-app.get("/api/timestamp/:date_string?", (req, res) => {
-  const input = req.params.date_string;
-  let date;
-
-  if (!input) {
-    date = new Date();
-  } else {
-    !isNaN(input)
-      ? (date = new Date(parseInt(input)))
-      : (date = new Date(input));
-  }
-
-  date == "Invalid Date"
-    ? res.json({ error: "Invalid Date" })
-    : res.json({ unix: date.getTime(), utc: date.toUTCString() });
+app.get("/api/whoami", (req, res) => {
+  res.json({
+    ipadress: req.header("host"),
+    language: req.header("accept-language"),
+    software: req.header("user-agent"),
+  });
 });
 
 app.listen(port, () => console.log(`Server runnig at port ` + port));
